a1dc167cb724864fe4ed06219fccd5b9f621486a
Michael Hatherly <michaelhatherly@gmail.com>
2020-09-08 16:56:33 +0100

    Revert "Update to 0.1.13."
    This reverts commit de26b59787d2a24b9af96be60c68a9eb6eb4dfe6.

--- build.jl
+++ build.jl
@@ -1,54 +1,70 @@
-version = "0.1.13"
-build = 1
-
 using Pkg.Artifacts
 using Pkg.BinaryPlatforms
-using URIParser
+using URIParser, FilePaths
 
-origin = "https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic@$(version)/"
+pkgname = "tectonic"
+origin = "https://github.com/tectonic-typesetting/tectonic/releases/download/continuous"
+version = v"0.1.13-dev"
+build = 1
+release_info = read(download(joinpath(origin, "RELEASE-INFO.txt")), String)
+commit = match(r"^commit=(.+)$"m, release_info)[1]
 
-artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
 build_path = joinpath(@__DIR__, "build")
 
-platforms = [
-    (Linux(:armv7l, libc=:musl, call_abi=:eabihf), "tectonic-$(version)-arm-unknown-linux-musleabihf.tar.gz"),
-    (Linux(:x86_64, libc=:glibc),                  "tectonic-$(version)-x86_64-unknown-linux-gnu.tar.gz"),
-    (Linux(:x86_64, libc=:musl),                   "tectonic-$(version)-x86_64-unknown-linux-musl.tar.gz"),
-    (MacOS(:x86_64),                               "tectonic-$(version)-x86_64-apple-darwin.tar.gz"),
-    (Windows(:x86_64),                             "tectonic-$(version)-x86_64-pc-windows-gnu.zip"),
-]
-
-@info "create build directory"
 ispath(build_path) && rm(build_path; recursive=true, force=true)
+
 mkpath(build_path)
 
-for (platform, file) in platforms
-    @info platform file
-    download_url = origin * file
-    download(download_url, file)
-    product_hash = create_artifact() do artifact_dir
-        if endswith(file, ".zip")
-            run(Cmd(`unzip $file -d $artifact_dir`))
-        else
-            run(Cmd(`tar -xvf $file -C $artifact_dir`))
+artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
+
+platforms = [
+    Linux(:x86_64),
+    MacOS(:x86_64),
+    Windows(:x86_64),
+]
+
+mktempdir() do temp_path
+    for platform in platforms
+        download_url =
+            if platform isa Linux
+                "tectonic-latest-x86_64-unknown-linux-musl.tar.gz"
+            elseif platform isa MacOS
+                "tectonic-latest-x86_64-apple-darwin.tar.gz"
+            elseif platform isa Windows
+                "tectonic-latest-x86_64-pc-windows-msvc.exe"
+            else
+                continue
+            end
+        download_url = joinpath(origin, download_url)
+        download_filename = Path(temp_path) / Path(basename(Path(URI(download_url).path)))
+
+        download(download_url, download_filename)
+
+        product_hash = create_artifact() do artifact_dir
+            if extension(download_filename) == "exe"
+                cp(string(download_filename), joinpath(artifact_dir, "tectonic.exe"))
+            else
+                run(Cmd(`tar -xvf $download_filename -C $artifact_dir`))
+            end
+
+            files = readdir(artifact_dir)
         end
-        files = readdir(artifact_dir)
+
+        archive_filename = "$pkgname-$commit-$(triplet(platform)).tar.gz"
+        download_hash = archive_artifact(product_hash, joinpath(build_path, archive_filename))
+
+        bind_artifact!(
+            artifact_toml,
+            "tectonic_bin",
+            product_hash,
+            platform=platform,
+            force=true,
+            download_info=Tuple[
+                (
+                 "https://github.com/MichaelHatherly/Tectonic.jl/releases/download/$(URIParser.escape("$(version)+$(build)"))/$archive_filename",
+                 download_hash
+                )
+            ]
+        )
     end
-    @info "building artifact"
-    archive_filename = "tectonic-$(version)-$(triplet(platform)).tar.gz"
-    download_hash = archive_artifact(product_hash, joinpath(build_path, archive_filename))
-    bind_artifact!(
-        artifact_toml,
-        "tectonic_bin",
-        product_hash,
-        platform=platform,
-        force=true,
-        download_info=Tuple[
-            (
-             "https://github.com/MichaelHatherly/Tectonic.jl/releases/download/$(URIParser.escape("$(version)+$(build)"))/$archive_filename",
-             download_hash
-            )
-        ]
-    )
-    rm(file; force=true)
 end
